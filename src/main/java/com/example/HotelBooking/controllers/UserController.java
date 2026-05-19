08f28d95c656c60cd7a47c7b4ef41c0a86d8ba1d
wesleyfernandesprojetofaculdade <wesley@viaapia.com.br>
2026-05-19 16:59:10 -0300

    user controller created

---
+++ src/main/java/com/example/HotelBooking/controllers/UserController.java
@@ -0,0 +1,49 @@
+package com.example.HotelBooking.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.HotelBooking.dtos.Response;
+import com.example.HotelBooking.dtos.UserDTO;
+import com.example.HotelBooking.services.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Response> updateUser(@RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.updateOwnAccount(userDTO));
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Response> deleteOwnAccount() {
+        return ResponseEntity.ok(userService.deleteOwnAccount());
+    }
+
+    @DeleteMapping("/account")
+    public ResponseEntity<Response> getOwnAccountDetails() {
+        return ResponseEntity.ok(userService.getOwnAccountDetails());
+    }
+
+    @GetMapping("/bookings")
+    public ResponseEntity<Response> getMyBookingHistory() {
+        return ResponseEntity.ok(userService.getMyBookingHistory());
+    }
+}
