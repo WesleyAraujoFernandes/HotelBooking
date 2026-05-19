40d16e6dd2145fe7d900770f2f01bb7efb497a1a
wesleyfernandesprojetofaculdade <wesley@viaapia.com.br>
2026-05-19 11:58:35 -0300

    send mail is working

--- src/main/java/com/example/HotelBooking/HotelBookingApplication.java
+++ src/main/java/com/example/HotelBooking/HotelBookingApplication.java
@@ -2,12 +2,36 @@ package com.example.HotelBooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import com.example.HotelBooking.dtos.NotificationDTO;
+import com.example.HotelBooking.notifications.NotificationService;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@EnableAsync
+@RequiredArgsConstructor
+@Slf4j
 public class HotelBookingApplication {
+	private final NotificationService notificationService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelBookingApplication.class, args);
+	}
+
+	@PostConstruct
+	public void sendDummyEmail() {
+		log.info("Send mail to:" + "wesley.a.fernandes@gmail.com");
+		NotificationDTO notificationDTO = NotificationDTO.builder()
+				.recipient("wesley.a.fernandes@gmail.com")
+				.subject("Test Email")
+				.body("This is a test email")
+				.build();
+
+		notificationService.sendEmail(notificationDTO);
 	}
 
 }
